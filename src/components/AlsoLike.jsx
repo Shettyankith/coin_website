a310fe2927518d75c79589bc497709cb392730a1
Ankith Shetty <shettyankith2003@gmail.com>
2025-01-13 10:33:00 +0530

    loading skeleton added

--- src/components/AlsoLike.jsx
+++ src/components/AlsoLike.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Coin from "./Coin";
 
 function AlsoLike() {
+  // Initialize emptyCoin with values (array of 16 items)
+  const emptyCoin = new Array(16).fill({});
+
   // State for trending coins
   const [trendingCoins, setTrendingCoins] = useState([]);
-  const [loading, setloading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   // Function to fetch trending coins
   const getTrendingCoins = async () => {
@@ -17,42 +19,62 @@ function AlsoLike() {
       );
       if (response.data.coins) {
         setTrendingCoins(response.data.coins);
-        setloading(false);
+        setLoading(false);
       }
     } catch (e) {
-      console.error("Error fetching aslo like coins:", e);
+      console.error("Error fetching also like coins:", e);
+      setLoading(false); 
     }
   };
 
   useEffect(() => {
     getTrendingCoins();
   }, []);
+
   return (
     <div className="bg-white w-full p-5 mt-5">
       <h2 className="text-black text-xl font-medium mb-4 capitalize">
         You may also like
       </h2>
       <div className="flex space-x-4 overflow-x-auto w-full">
-        {loading ? (
-          <p>Loading</p>
-        ) : (
-          trendingCoins.map((coin, index) => (
-            <Coin coin={coin.item} key={index} />
-          ))
-        )}
+        {loading
+          ? emptyCoin.map((coin, index) => (
+              <div
+                key={index}
+                className="border-gray-200 p-4 border-[1px] flex flex-col justify-around items-center rounded-md min-w-[250px] w-[fit-content] overflow-hidden"
+              >
+                <div className="flex space-x-2 items-center justify-even shrink-0 w-full">
+                  <p className="w-[50px] h-[50px] rounded-full object-cover mr-1 px-5 py-4 bg-slate-200 animate-pulse"></p>
+                  <p className="bg-slate-200 animate-pulse p-3 w-[80%]"></p>
+                  <button className="text-sm lg:text-[15px] rounded-md bg-slate-200 animate-pulse p-3 px-5"></button>
+                </div>
+                <h4 className="font-medium justify-self-start bg-slate-200 animate-pulse py-3 my-2 w-full"></h4>
+                <p className="w-[220px] h-[80px] object-cover animate-pulse bg-slate-200"></p>
+              </div>
+            ))
+          : trendingCoins.map((coin, index) => <Coin coin={coin.item} key={index} />)}
       </div>
 
       <h2 className="text-black text-xl font-medium my-4 capitalize">
         Trending Coins
       </h2>
       <div className="flex space-x-4 overflow-x-auto w-full">
-        {loading ? (
-          <p>Loading</p>
-        ) : (
-          trendingCoins.map((coin, index) => (
-            <Coin coin={coin.item} key={index} />
-          ))
-        )}
+        {loading
+          ? emptyCoin.map((coin, index) => (
+              <div
+                key={index}
+                className="border-gray-200 p-4 border-[1px] flex flex-col justify-around items-center rounded-md min-w-[250px] w-[fit-content] overflow-hidden"
+              >
+                <div className="flex space-x-2 items-center justify-even shrink-0 w-full">
+                  <p className="w-[50px] h-[50px] rounded-full object-cover mr-1 px-5 py-4 bg-slate-200 animate-pulse"></p>
+                  <p className="bg-slate-200 animate-pulse p-3 w-[80%]"></p>
+                  <button className="text-sm lg:text-[15px] rounded-md bg-slate-200 animate-pulse p-3 px-5"></button>
+                </div>
+                <h4 className="font-medium justify-self-start bg-slate-200 animate-pulse py-3 my-2 w-full"></h4>
+                <p className="w-[220px] h-[80px] object-cover animate-pulse bg-slate-200"></p>
+              </div>
+            ))
+          : trendingCoins.map((coin, index) => <Coin coin={coin.item} key={index} />)}
       </div>
     </div>
   );
